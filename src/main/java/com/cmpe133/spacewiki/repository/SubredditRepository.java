1edd7676344e370d8341d1cdc6670650047aebac
Rohan Surana <rohansurana2810@gmail.com>
2021-04-19 17:48:17 +0530

    repository

---
+++ src/main/java/com/cmpe133/spacewiki/repository/SubredditRepository.java
@@ -0,0 +1,13 @@
+package com.cmpe133.spacewiki.repository;
+
+import com.cmpe133.spacewiki.model.Subreddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
+
+    Optional<Subreddit> findByName(String subredditName);
+}
---
+++ src/main/java/com/cmpe133/spacewiki/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cmpe133.spacewiki.repository;
+
+import com.cmpe133.spacewiki.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
