411f997872fd8423ba036fbf36b174bad12183a4
Rohan Surana <rohansurana2810@gmail.com>
2021-04-19 17:01:34 +0530

    init

---
+++ src/main/java/com/cmpe133/spacewiki/SpacewikiApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe133.spacewiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacewikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpacewikiApplication.class, args);
+    }
+
+}
