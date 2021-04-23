b85e3ac3c3614c2192d53c3c826625005a979b5b
Rohan Surana <rohansurana2810@gmail.com>
2021-04-23 19:28:47 +0530

    create and fetch comments api

--- src/main/java/com/cmpe133/spacewiki/repository/CommentRepository.java
+++ src/main/java/com/cmpe133/spacewiki/repository/CommentRepository.java
@@ -1,9 +1,17 @@
 package com.cmpe133.spacewiki.repository;
 
 import com.cmpe133.spacewiki.model.Comment;
+import com.cmpe133.spacewiki.model.Post;
+import com.cmpe133.spacewiki.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
 }
+
---
+++ src/main/java/com/cmpe133/spacewiki/service/CommentService.java
@@ -0,0 +1,62 @@
+package com.cmpe133.spacewiki.service;
+
+import com.cmpe133.spacewiki.dto.CommentsDto;
+import com.cmpe133.spacewiki.exception.PostNotFoundException;
+import com.cmpe133.spacewiki.mapper.CommentMapper;
+import com.cmpe133.spacewiki.model.Comment;
+import com.cmpe133.spacewiki.model.NotificationEmail;
+import com.cmpe133.spacewiki.model.Post;
+import com.cmpe133.spacewiki.model.User;
+import com.cmpe133.spacewiki.repository.CommentRepository;
+import com.cmpe133.spacewiki.repository.PostRepository;
+import com.cmpe133.spacewiki.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+    private static final String POST_URL = "";
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
+
+    public void save(CommentsDto commentsDto) {
+        Post post = postRepository.findById(commentsDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
+        Comment comment = commentMapper.map(commentsDto, post, authService.getCurrentUser());
+        commentRepository.save(comment);
+
+        String message = mailContentBuilder.build(authService.getCurrentUser() + " posted a comment on your post." + POST_URL);
+        sendCommentNotification(message, post.getUser());
+    }
+
+    private void sendCommentNotification(String message, User user) {
+        mailService.sendMail(new NotificationEmail(user.getUsername() + " Commented on your post", user.getEmail(), message));
+    }
+
+    public List<CommentsDto> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException(postId.toString()));
+        return commentRepository.findByPost(post)
+                .stream()
+                .map(commentMapper::mapToDto).collect(toList());
+    }
+
+    public List<CommentsDto> getAllCommentsForUser(String userName) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(userName));
+        return commentRepository.findAllByUser(user)
+                .stream()
+                .map(commentMapper::mapToDto)
+                .collect(toList());
+    }
+}
